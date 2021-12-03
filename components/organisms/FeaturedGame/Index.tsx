37f53a1d2b1827fa88ebf00cd8f97927916f027f
BrondoL <nabilunited2@gmail.com>
2021-12-03 16:28:18 +0700

    Add api service

--- components/organisms/FeaturedGame/Index.tsx
+++ components/organisms/FeaturedGame/Index.tsx
@@ -1,6 +1,18 @@
+import { useCallback, useEffect, useState } from "react";
 import GameItem from "../../molecules/GameItem/Index";
+import { getFeaturedGame } from "../../../services/player";
 
 export default function FeaturedGame() {
+    const [gameList, setGameList] = useState([]);
+
+    const getFeaturedGameList = useCallback(async () => {
+        const data = await getFeaturedGame();
+        setGameList(data);
+    }, [getFeaturedGame]);
+
+    useEffect(() => {
+        getFeaturedGameList();
+    }, []);
     return (
         <section className="featured-game pt-50 pb-50">
             <div className="container-fluid">
@@ -8,11 +20,11 @@ export default function FeaturedGame() {
                 </h2>
                 <div className="d-flex flex-row flex-lg-wrap overflow-setting justify-content-lg-between gap-lg-3 gap-4"
                     data-aos="fade-up">
-                    <GameItem title="Super Mechs" category="Mobile" thumbnail="/img/Thumbnail-1.png" />
-                    <GameItem title="Call of Duty: Modern" category="Mobile" thumbnail="/img/Thumbnail-2.png" />
-                    <GameItem title="Mobile Legends" category="Mobile" thumbnail="/img/Thumbnail-3.png" />
-                    <GameItem title="Clash of Clans" category="Mobile" thumbnail="/img/Thumbnail-4.png" />
-                    <GameItem title="Valorant" category="Desktop" thumbnail="/img/Thumbnail-5.png" />
+                        {gameList.map((item) => {
+                            return (
+                                <GameItem key={item._id} title={item.name} category={item.category.name} thumbnail={`https://brondol-store-gg.herokuapp.com/uploads/${item.thumbnail}`} />
+                            )
+                        })}
                 </div>
             </div>
         </section>
